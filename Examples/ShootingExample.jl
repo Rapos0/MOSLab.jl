ce5a8d5f7c43d7909a77b1ba41a4e2d5564c3be4
Rapos0 <jrraposo1@gmail.com>
2021-09-08 13:19:27 +0200

    Circuit Creation

--- Examples/ShootingExample.jl
+++ Examples/ShootingExample.jl
@@ -1,11 +1,6 @@
 using MOSLab
-using Plots
+using Revise
 
-N_d = 6e16
-tox = 4e-7
-T = 300.0
-Nb = 5.7e17
-l = 1e-4
-h = 1e-4
-MI = MOSFETInputDeck(N_d,Nb,T,l,h,0.5e-4,0.5e-4,tox,:NMOS)
-G = Grid(MI,101,101)
+
+ckt = Circuit()
+
