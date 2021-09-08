9edaaa379fd4698c14bedabea7cedb7f79fedff4
Rapos0 <jrraposo1@gmail.com>
2021-09-08 13:19:39 +0200

    Update Circuit.jl

--- src/Circuit.jl
+++ src/Circuit.jl
@@ -1,11 +1,12 @@
 
 
 struct CircuitComponent
+    name::String
     component::Component
     pins::Dict{String,Int}
 end
 
-CircuitComponent(comp::TransistorModel) = CircuitComponent(comp,["d"=>1,"s"=>2,"g"=>3])
+CircuitComponent(comp::TransistorModel) = CircuitComponent(name,comp,["d"=>1,"s"=>2,"g"=>3])
 
 mutable struct Circuit
     components::AbstractVector{CircuitComponent}
@@ -16,11 +17,14 @@ end
 
 add_node!(ckt::Circuit,node::Pair{String,Int}) = push!(ckt.nodes,node) 
 
-Circuit() = Circuit([],[[]],[],[])
+Circuit() = Circuit([],zeros(Bool,1,1),Dict("gnd!"=> 0),[])
 
-# function add_component(ckt::Circuit,comp::TransistorModel,connections::Dict{String,Int})
-#     for node in connections
-#         if node[2] ∉ ckt.nodes
-#             add_node!(ckt,node)
-#     end
-# end
+function add_component(ckt::Circuit,comp::TransistorModel,connections::Dict{String,Int})
+    #Add Nodes to Circuit if they don't exists already
+    for node in connections
+        if node[2] ∉ ckt.nodes
+            add_node!(ckt,node)
+        end
+    end
+
+end
