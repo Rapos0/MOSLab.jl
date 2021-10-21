97b3351b2d728a13afa376c1de26ab552b41340f
Documenter.jl <documenter@juliadocs.github.io>
2021-10-21 14:13:00 +0000

    build based on f55ab22

---
+++ 0.1.1-zenodo/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MOSLab.jl","page":"MOSLab.jl","title":"MOSLab.jl","text":"","category":"section"},{"location":"","page":"MOSLab.jl","title":"MOSLab.jl","text":"Documentation for MOSLab.jl","category":"page"}]
+}
